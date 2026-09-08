9c78a8f31b473f3ead2770cae35c9500bbf5ff24
embabyty <ios11emiry@gmail.com>
2026-09-08 16:15:17 -0500

    Vendor tweak: custom login, floating nav bar with Material icons, M logo, premium paywall
    - Vendor the NeoFreeBird tweak source into this repo (workflow now builds from main/tweak instead of a separate repo)
    - Add custom xAuth login screen (LegacyLogin) to bypass Twitter Attestation sign-in block
    - Add floating frosted-glass navigation pill replacing the stock tab bar (Material 3 icons, accent-tinted, respects saved tabs)
    - Replace home/opening logos with the custom M logo, tinted with the accent color
    - Keep EMTwitter Premium paywall (full-screen, Patreon unlock)

---
+++ tweak/BHTManager.h
@@ -0,0 +1,101 @@
+//
+//  BHTdownloadManager.h
+//  BHT
+//
+//  Created by BandarHelal on 24/12/1441 AH.
+//
+
+#import "TWHeaders.h"
+
+
+@interface BHTManager : NSObject
++ (NSString *)getDownloadingPersent:(float)per;
++ (void)cleanCache;
++ (NSString *)getVideoQuality:(NSString *)url;
++ (BOOL)isVideoCell:(id <T1StatusViewModel>)model;
++ (bool)isDMVideoCell:(T1InlineMediaView *)view;
++ (BOOL)doesContainDigitsOnly:(NSString *)string;
++ (UIViewController *)BHTSettingsWithAccount:(TFNTwitterAccount *)twAccount;
++ (void)showSaveVC:(NSURL *)url;
++ (void)save:(NSURL *)url;
++ (MediaInformation *)getM3U8Information:(NSURL *)mediaURL;
++ (TFNMenuSheetViewController *)newFFmpegDownloadSheet:(MediaInformation *)mediaInformation downloadingURL:(NSURL *)downloadingURL progressView:(JGProgressHUD *)hud;
+
++ (BOOL)DownloadingVideos;
++ (BOOL)DirectSave;
++ (BOOL)UndoTweet;
++ (BOOL)NoHistory;
++ (BOOL)BioTranslate;
++ (BOOL)LikeConfirm;
++ (BOOL)TweetConfirm;
++ (BOOL)FollowConfirm;
++ (BOOL)HidePromoted;
++ (BOOL)HideTopics;
++ (BOOL)DisableVODCaptions;
++ (BOOL)Padlock;
++ (BOOL)OldStyle;
++ (BOOL)changeFont;
++ (BOOL)FLEX;
++ (BOOL)autoHighestLoad;
++ (BOOL)disableSensitiveTweetWarnings;
++ (BOOL)showScrollIndicator;
++ (BOOL)CopyProfileInfo;
++ (BOOL)tweetToImage;
++ (BOOL)hideSpacesBar;
++ (BOOL)disableRTL;
++ (BOOL)alwaysOpenSafari;
++ (BOOL)hideWhoToFollow;
++ (BOOL)hideTopicsToFollow;
++ (BOOL)hideBlueVerified;
++ (BOOL)hideViewCount;
++ (BOOL)hidePremiumOffer;
++ (BOOL)hideTrendVideos;
++ (BOOL)forceTweetFullFrame;
++ (BOOL)stripTrackingParams;
++ (BOOL)alwaysFollowingPage;
++ (BOOL)stopHidingTabBar;
++ (BOOL)changeBackground;
++ (bool)backgroundImage;
++ (BOOL)hideBookmarkButton;
++ (BOOL)customVoice;
++ (BOOL)RestoreTweetLabels;
++ (BOOL)disableMediaTab;
++ (BOOL)disableArticles;
++ (BOOL)disableHighlights;
+
++ (BOOL)hideGrokAnalyze;
++ (BOOL)hideFollowButton;
++ (BOOL)restoreFollowButton;
++ (BOOL)squareAvatars;
++ (BOOL)restoreVideoTimestamp;
++ (BOOL)dmAvatars;
++ (BOOL)classicTabBarEnabled;
++ (BOOL)restoreTabLabels;
++ (BOOL)noTabBarHiding;
+
+// Translate functionality
++ (BOOL)enableTranslate;
++ (NSString *)translateEndpoint;
++ (NSString *)translateAPIKey;
++ (NSString *)translateModel;
+
++ (BOOL)dmComposeBarV2;
++ (BOOL)replySorting;
++ (BOOL)dmVoiceCreation;
+
++ (void)clearSourceLabelCache;
+
++ (BOOL)restoreReplyContext;
++ (BOOL)disableXChat;
+
+// EMTwitter Premium paywall
++ (BOOL)Premium;
++ (void)setPremium:(BOOL)unlocked;
++ (NSString *)premiumEmail;
++ (void)setPremiumEmail:(NSString *)email;
++ (NSString *)premiumPatreonURL;
++ (NSArray *)premiumEmails;
++ (BOOL)isPremiumEmail:(NSString *)email;
+
+@end
+
